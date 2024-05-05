509cc4b0d6efc7e3490a3d5835ff61b823b989ca
LuK1337 <priv.luk@gmail.com>
2024-05-05 11:04:57 +0200

    avbtool: Add retry to generate_fec_data()
    When signing multiple builds at once, we can run out of memory during
    this step, let's just attempt to retry it up to 3 times when that
    happens.
    
    Change-Id: I308b37823ea4682fb14181735d94be02eb916e5b

--- avbtool.py
+++ avbtool.py
@@ -31,6 +31,7 @@ import hashlib
 import json
 import math
 import os
+import signal
 import struct
 import subprocess
 import sys
@@ -4105,7 +4106,7 @@ def calc_fec_data_size(image_size, num_roots):
   return int(pout)
 
 
-def generate_fec_data(image_filename, num_roots):
+def generate_fec_data(image_filename, num_roots, attempt=1):
   """Generate FEC codes for an image.
 
   Arguments:
@@ -4125,6 +4126,8 @@ def generate_fec_data(image_filename, num_roots):
            fec_tmpfile.name],
           stderr=open(os.devnull, 'wb'))
     except subprocess.CalledProcessError as e:
+      if attempt < 3 and e.returncode == -signal.SIGKILL:
+        return generate_fec_data(image_filename, num_roots, attempt + 1)
       raise ValueError('Execution of \'fec\' tool failed: {}.'
                        .format(e)) from e
     fec_data = fec_tmpfile.read()
